87bed82cba2c3b420d2f07bbf3edca8703a8e094
ParthaManna <manna79partha@gmail.com>
2022-04-28 15:57:20 +0530

    database updated

--- jobapp/models.py
+++ jobapp/models.py
@@ -21,7 +21,6 @@ class Candidate(models.Model):
     address=models.TextField()
     dob=models.CharField(max_length=15)
     gender=models.CharField(max_length=15)
-    profilePic=models.ImageField(upload_to="jobapp/img/candidate")
 
 class Company(models.Model):
     userid=models.ForeignKey(UserMaster,on_delete=models.CASCADE)
@@ -32,10 +31,10 @@ class Company(models.Model):
     city=models.CharField(max_length=20)
     address=models.TextField()
     contact=models.CharField(max_length=15)
-    logo=models.ImageField(upload_to="jobapp/img/company")
     
 class JobDetails(models.Model):
     userid=models.ForeignKey(UserMaster,on_delete=models.CASCADE)
+    companylogo=models.ImageField(upload_to="jobapp/img/company")
     jobname=models.CharField(max_length=200)
     jobtype=models.CharField(max_length=30)
     companyname=models.CharField(max_length=100)
@@ -44,7 +43,31 @@ class JobDetails(models.Model):
     responsibilities=models.TextField()
     location=models.CharField(max_length=50)
     website=models.CharField(max_length=50)
-    companyemail=models.CharField(max_length=50)
+    companyemail=models.EmailField()
     salary=models.CharField(max_length=20)
     experience=models.CharField(max_length=20)
     requirments=models.TextField()
+    lastdate=models.CharField(max_length=15)
+
+class Candidate_resume(models.Model):
+    resumeid=models.ForeignKey(UserMaster,on_delete=models.CASCADE)
+    linkdin=models.CharField(max_length=150)
+    github=models.CharField(max_length=150)
+    fullname=models.CharField(max_length=40)
+    email=models.EmailField()
+    contact=models.CharField(max_length=12)
+    collegename=models.CharField(max_length=60)
+    course=models.CharField(max_length=50)
+    passingyear=models.CharField(max_length=6)
+    skills=models.CharField(max_length=1000)
+    experience=models.CharField(max_length=10)
+
+class Jobapplications(models.Model):
+    jobid=models.ForeignKey(Candidate,on_delete=models.CASCADE)
+    fullname=models.CharField(max_length=40)
+    email=models.EmailField()
+    contact=models.CharField(max_length=12)
+    experience=models.CharField(max_length=10)
+    resume=models.ForeignKey(Candidate_resume,on_delete=models.CASCADE)
+
+
